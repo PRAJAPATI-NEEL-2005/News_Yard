06a85330129be82229d00fe694f3ef4db7c31777
Neel-H-Prajapati <prajapatinimisha853@gmail.com>
2025-06-28 22:19:06 +0530

    made some css change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">NewsYard</Link>
+    <Link className="navbar-brand" to="/"> <strong>NewsYard</strong></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
