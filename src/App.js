51669e1cdcad5fefa4e5f9f0721a1c4d04b7cde5
Neel-H-Prajapati <prajapatinimisha853@gmail.com>
2025-06-28 22:16:52 +0530

    made package change

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 import LoadingBar from "react-top-loading-bar";
+
 import {
   BrowserRouter as Router,
   Routes,
@@ -26,6 +27,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
+      
         <Router>
             <LoadingBar
         color="#f11946"
